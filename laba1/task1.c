660b004ab902da78dbd18645b1342d68ad4de691
Max <lastochkin.maks05@mail.ru>
2024-09-17 00:48:44 +0300

    task1 2.0

--- laba1/task1.c
+++ laba1/task1.c
@@ -1,36 +1,47 @@
 #include <stdio.h>
-#include <string.h>
 #include <limits.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <math.h>
+#include <string.h>
 
 long long int factorial(long long int number);
 void sys_of_calc(long long int number);
 long long int my_pow(long long int footing, long long int degree);
+int str_to_lli(const char *str, long long int *x, int system);
 
-
-int main() {
+int main(int args, char *argv[]) {
+    errno = 0;
     long long int x;
-    char flag[10];
 
-    if(2 != scanf("%lli %2s", &x, flag) || (x == LLONG_MAX)) {
+    if (args != 3) {
         printf("input error\n");
         return -1;
     }
+
+    if(!((argv[1][0] == '-' || argv[1][0] == '/') && argv[1][2] == '\0') ) {
+        printf("input error\n");
+        return -1;
+    }
+
+    if (str_to_lli(argv[2], &x, 10) != 0) {
+        printf("input error\n");
+        return -1;
+    }
+
     if (x <= 0) {
         printf("Error a negative number is not suitable for processing\n");
         return -1;
     }
 
-    printf("%lli %s\n", x, flag);
-
-    if (strcmp(flag, "-h") == 0 || strcmp(flag, "/h") == 0) {
+    if (argv[1][1] == 'h') {
         if (x > 100) {
             printf ("There are no such numbers\n");
         } else {
             int count = 0;
             for (int i = 1; i <= 100; ++i) {
                 if (i % x == 0) {
-                    printf("%d ", i);
+                    printf("Answer: %d ", i);
                     count += 1;
                 }
             }
@@ -41,7 +52,7 @@ int main() {
 
         }
 
-    } else if (strcmp(flag, "-p") == 0 || strcmp(flag, "/p") == 0) {
+    } else if (argv[1][1] == 'p') {
 
         for (int i = 2; i < x; ++i) {
             if (x % i == 0) {
@@ -51,11 +62,11 @@ int main() {
         }
         printf("the number is simple\n");
 
-    } else if (strcmp(flag, "-s") == 0 || strcmp(flag, "/s") == 0) {
+    } else if (argv[1][1] == 's') {
 
         sys_of_calc(x);
 
-    } else if (strcmp(flag, "-e") == 0 || strcmp(flag, "/e") == 0) {
+    } else if (argv[1][1] == 'e') {
         if (x > 10) {
             printf ("An invalid number for this flag\n");
         } else {
@@ -76,7 +87,7 @@ int main() {
             }
 
         }
-    } else if (strcmp(flag, "-a") == 0 || strcmp(flag, "/a") == 0) {
+    } else if (argv[1][1] == 'a') {
         long long int sum = 0;
         for (long long int i = 0; i <= x; ++i) {
             if ((unsigned long long int)(sum + i) > LLONG_MAX) {
@@ -85,14 +96,14 @@ int main() {
             }
             sum += i;
         }
-        printf("Sum: %lli", sum);
+        printf("Sum: %lli\n", sum);
 
-    } else if (strcmp(flag, "-f") == 0 || strcmp(flag, "/f") == 0) {
+    } else if (argv[1][1] == 'f') {
 
         if (factorial(x) == -1){
             printf("type overflow\n");
         }else {
-            printf("%lld", factorial(x));
+            printf("Factorial: %lld\n", factorial(x));
         }
 
     } else {
@@ -135,7 +146,31 @@ long long int my_pow(long long int footing, long long int degree) {
 }
 
 void sys_of_calc(long long int number) {
+
     char hex_str[100];
     sprintf(hex_str, "%llx", number);
-    printf("Hex answer: %s\n", hex_str);
+
+    unsigned long long int n = strlen(hex_str);
+    printf("Hex answer: ");
+
+    for (unsigned long long int i = 0; i < n; ++i) {
+        printf("%c ", hex_str[i]);
+    }
+    printf("\n");
+}
+
+int str_to_lli(const char *str, long long int *x, int system) {
+
+    char *end;
+    *x = strtoll(str, &end, system);
+
+    if ((*x == LLONG_MAX || *x == LLONG_MIN) && (errno == ERANGE)) {
+        return -1;
+    } else if (errno != 0 && *x == 0) {
+        return -1;
+    } else if (*end != '\0') {
+        return -1;
+    } else {
+        return 0;
+    }
 }
